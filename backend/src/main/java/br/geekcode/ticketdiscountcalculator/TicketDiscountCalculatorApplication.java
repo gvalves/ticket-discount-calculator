a636b2a0931172bb4efbd0e31f5d1796d32dd0dd
gvalves <gva@geekcode.com.br>
2023-02-24 21:49:58 -0300

    Project init

---
+++ backend/src/main/java/br/geekcode/ticketdiscountcalculator/TicketDiscountCalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.geekcode.ticketdiscountcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDiscountCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketDiscountCalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/geekcode/ticketdiscountcalculator/TicketDiscountCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.geekcode.ticketdiscountcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDiscountCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
